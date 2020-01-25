dc17060e6a84a22c0ed9b669fe9c218c259288dd
Vladimir Shitov <35199218+VladimirShitov@users.noreply.github.com>
2020-01-25 19:30:29 +0800

    Added a newer version
    You can find the history of changes in easy_3d repository

---
+++ easy_3d/carousel.js
@@ -0,0 +1,53 @@
+// carousel
+var images = document.getElementsByClassName('my-carousel-img');
+var i = 0;
+
+change_image = function(is_forward) {
+	current_image = images[i];
+	current_image.classList.remove("active");
+	current_image.classList.add("inactive");
+
+	if (is_forward) {
+		i = (i < images.length - 1) ? i+1 : 0;
+	} else {
+		i = (i > 0) ? i-1 : images.length - 1;
+	}
+
+	next_image = images[i];
+	next_image.classList.remove("inactive");
+	next_image.classList.add("active");
+}
+
+carousel = document.getElementById("my-carousel");
+
+var x;
+var y;
+
+const OFFSET = 25;
+
+carousel.ondragstart = function() {
+	return false;
+}
+
+carousel.onmousedown = function(e) {
+	x = e.pageX;
+	y = e.pageY;
+
+	carousel.onmousemove = function(e) {
+		if(e.pageX - x > OFFSET) {
+			change_image(true);
+			x = e.pageX;
+			y = e.pageY;
+		}
+		else if(e.pageX - x < -OFFSET) {
+			change_image(false);
+			x = e.pageX;
+			y = e.pageY;
+		}
+	}
+
+	document.onmouseup = function() {
+		carousel.onmousemove = null;
+	}
+
+}
